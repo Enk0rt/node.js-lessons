27cfb063733d30d904216ea9ffc5f3227974454e
Enk0rt <delovoyartem4ick@gmail.com>
2025-03-05 22:36:36 +0200

    update - fix hw1

--- main.js
+++ main.js
@@ -1,40 +1,30 @@
 const start = async () => {
     const path = require('path')
     const fs = require("fs")
+    const afs = require("node:fs/promises")
     const readLine = require('readline')
 
-    const emails = path.join(process.cwd(), 'storage', 'emails.txt')
+    const source = path.join(process.cwd(), 'storage', 'emails.txt')
 
-    const gmailCom = path.join(process.cwd(), 'gmail.txt')
-    const mailRu = path.join(process.cwd(), 'mail.txt')
-    const plusgmailRu = path.join(process.cwd(), 'plusgmail.txt')
-
-    const fileStream = fs.createReadStream(emails, 'utf-8')
+    const fileStream = fs.createReadStream(source, 'utf-8')
     const rl = readLine.createInterface({input: fileStream})
 
     try {
-        for await (const line of rl) {
-            const mail = /\b[A-Za-z0-9._%+-]+@mail\.ru\b/
-            const gmail = /\b[A-Za-z0-9._%+-]+@gmail\.com\b/
-            const plusGmail = /\b[A-Za-z0-9._%+-]+@plusgmail\.ru\b/
 
-            if (line.match(mail)) {
-                await fs.appendFile(mailRu, line+'\n', () => {
-                    console.log(`${line}\n`)
-                })
-            } else if (line.match(gmail)) {
-                await fs.appendFile(gmailCom, line+'\n', () => {
-                    console.log(`${line}\n`)
-                })
-            } else if (line.match(plusGmail)) {
-                await fs.appendFile(plusgmailRu, line+'\n', () => {
-                    console.log(`${line}\n`)
-                })
-            }
+        for await (const line of rl) {
+            const email = line.split('\t').splice(-1)[0]
+            const emailSplit = email.split('@')
+
+            if (emailSplit.length !== 2) continue
+
+            const domainName = emailSplit.splice(-1)[0]
+            const fileName = `${domainName}.txt`
+            console.log(email)
+            await afs.appendFile(path.join(process.cwd(), 'emails', fileName), `${email}\n`)
+
         }
-    }
-    finally
-    {
+
+    } finally {
         rl.close()
     }
 }
